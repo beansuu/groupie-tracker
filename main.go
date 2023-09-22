2277007e3321d590df6f8477ea198191cc22600f
mtalli <mauno.talli@hotmail.com>
2023-09-22 16:04:18 +0300

    Added dates, relation that does the link between artists, dates and locations

--- main.go
+++ main.go
@@ -21,6 +21,18 @@ type Location struct {
 	UpcomingConcert string `json:"upcoming_concert"`
 }
 
+type Date struct {
+	ID              string `json:"id"`
+	LastConcert     string `json:"last_concert"`
+	UpcomingConcert string `json:"upcoming_concert"`
+}
+
+type Relation struct {
+	ArtistID   string `json:"artist_id"`
+	LocationID string `json:"location_id"`
+	DateID     string `json:"date_id"`
+}
+
 // artists slice
 var artists = []Artist{
 	{
@@ -40,6 +52,22 @@ var locations = []Location{
 	},
 }
 
+var dates = []Date{
+	{
+		ID:              "1",
+		LastConcert:     "2022-09-15",
+		UpcomingConcert: "2023-10-20",
+	},
+}
+
+var relations = []Relation{
+	{
+		ArtistID:   "1",
+		LocationID: "1",
+		DateID:     "1",
+	},
+}
+
 func main() {
 	// Serve static assets like CSS
 	fs := http.FileServer(http.Dir("static"))
@@ -53,6 +81,10 @@ func main() {
 
 	// Route for home page
 	http.HandleFunc("/", homeHandler)
+
+	http.HandleFunc("/dates", datesHandler)
+
+	http.HandleFunc("/relations", relationsHandler)
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -88,6 +120,28 @@ func locationsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := json.NewEncoder(w).Encode(locations); err != nil {
+		http.Error(w, "Failed to encode data", http.StatusInternalServerError)
+	}
+}
+
+func datesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(dates); err != nil {
+		http.Error(w, "Failed to encode data", http.StatusInternalServerError)
+	}
+}
+
+func relationsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(relations); err != nil {
 		http.Error(w, "Failed to encode data", http.StatusInternalServerError)
 	}
 }
